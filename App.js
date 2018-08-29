0aefc65e4ab63e6493340359f536d70cf02e58d3
Jonathan <jonathan.che@outlook.com>
2018-08-28 19:22:46 -0700

    initial commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import FrontStack from './components/RootStack';
+
+export default class App extends Component {
+  render() {
+    return <FrontStack />;
+  }
+}
